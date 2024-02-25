16bd5e4f1003e6029f4d709f0f14f9f19ce26490
Thayumanavan Arulnambi <thayumanavan6@gmail.com>
2024-02-25 17:44:15 +0530

    changing RTL query

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -2,13 +2,12 @@ import {render,screen}  from '@testing-library/react';
 import Login from '../Login';
 
 test('checking component loaded',() =>{
-    // steps for testing heading element loaded
     render(<Login/>);
-    expect(screen.queryByText(/Login/)).toBeInTheDocument();
+    expect(screen.getByText(/Login/)).toBeInTheDocument();
 })
 
 test('checking email & password inputs are empty', () => {
     render(<Login/>);
-    expect(screen.queryByPlaceholderText('Email')).toHaveValue("");
-    expect(screen.queryByPlaceholderText('Password')).toHaveValue("");
+    expect(screen.getByPlaceholderText('Email')).toHaveValue("");
+    expect(screen.getByPlaceholderText('Password')).toHaveValue("");
 })
